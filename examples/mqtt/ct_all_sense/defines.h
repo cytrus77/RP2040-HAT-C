7fbda9122b235c1ad20437fd8c612a1ebf0ca73d
cytrus77 <cytrus77@gmail.com>
2024-02-11 21:05:29 +0100

    ct pico all sense

---
+++ examples/mqtt/ct_all_sense/defines.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Macros
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+#define PLL_SYS_KHZ (133 * 1000)
+
+/* Buffer */
+#define ETHERNET_BUF_MAX_SIZE (1024 * 2)
+
+/* Socket */
+#define SOCKET_MQTT 0
+
+/* Timeout */
+#define DEFAULT_TIMEOUT 1000 // 1 second
+
+
+// Ethernet config
+// Bolus Ratusz
+#define MAC      { 0x00, 0x08, 0xDC, 0x45, 0xA8, 0x11 }
+#define IP       { 192, 168, 89, 101 }
+#define SUBNET   { 255, 255, 254, 0 }
+#define GATEWAY  { 192, 168, 88, 1 }
+#define MQTT_SERVER { 192, 168, 89, 100 }
+// WLKP
+// #define MAC      { 0x00, 0x08, 0xDC, 0xE8, 0xA8, 0x11 }
+// #define IP       { 192, 168, 116, 199 }
+// #define SUBNET   { 255, 255, 255, 0 }
+// #define GATEWAY  { 192, 168, 116, 1 }
+// #define MQTT_SERVER { 192, 168, 116, 135 }
+#define DNS      { 8, 8, 8, 8 }
+
+/* MQTT IP */
+#define MQTT_PUBLISH_PERIOD (1000 * 1) // 1 seconds
+#define MQTT_KEEP_ALIVE 10 // milliseconds
+const uint16_t MQTT_PORT = 1883;
+
+// MQTT
+const string MQTT_CLIENT_ID = "rpi-pico";
+const string MQTT_USERNAME = "wiznet";
+const string MQTT_PASSWORD = "0123456789";
+
+const string deviceName = "CT01Alarm";
+const string cmndSufix  = "/cmnd";
+
+const string statusTopic      = deviceName + "/status";
+const string uptimeTopic      = deviceName + "/uptime";
+const string willTopic        = deviceName + "/LWT";
+const string willMessageOff   = "offline";
+const string willMessageOn    = "online";
+
+const string In1A_Topic       = deviceName + "/in1a";
+const string In1B_Topic       = deviceName + "/in1b";
+const string In2A_Topic       = deviceName + "/in2a";
+const string In2B_Topic       = deviceName + "/in2b";
+const string In3A_Topic       = deviceName + "/in3a";
+const string In3B_Topic       = deviceName + "/in3b";
+const string In4A_Topic       = deviceName + "/in4a";
+const string In4B_Topic       = deviceName + "/in4b";
+const string In5A_Topic       = deviceName + "/in5a";
+const string In5B_Topic       = deviceName + "/in5b";
+const string In6A_Topic       = deviceName + "/in6a";
+const string In6B_Topic       = deviceName + "/in6b";
+const string In7A_Topic       = deviceName + "/in7a";
+const string In7B_Topic       = deviceName + "/in7b";
+const string In8A_Topic       = deviceName + "/in8a";
+const string In8B_Topic       = deviceName + "/in8b";
+const string In9A_Topic       = deviceName + "/in9a";
+const string In9B_Topic       = deviceName + "/in9b";
+
+const int In1aPin = 2;
+const int In1bPin = 3;
+const int In2aPin = 4;
+const int In2bPin = 5;
+const int In3aPin = 6;
+const int In3bPin = 7;
+const int In4aPin = 8;
+const int In4bPin = 9;
+const int In5aPin = 10;
+const int In5bPin = 11;
+const int In6aPin = 12;
+const int In6bPin = 13;
+const int In7aPin = 28;
+const int In7bPin = 14;
+const int In8aPin = 27;
+const int In8bPin = 15;
+const int In9aPin = 26;
+const int In9bPin = 22;
+
+
+typedef struct Input
+{
+    bool initDone;
+    bool measuredValue;
+    bool lastReportedValue;
+    const string& topicStat;
+    const uint8_t pinNo;
+
+    Input(const string& _topicStat, const uint8_t _pinNo)
+     : initDone(false) 
+     , measuredValue(false)
+     , lastReportedValue(false)
+     , topicStat(_topicStat)
+     , pinNo(_pinNo)
+    {}
+} Input;
+
+
+Input in1a = Input(In1A_Topic, In1aPin);
+Input in1b = Input(In1B_Topic, In1bPin);
+Input in2a = Input(In2A_Topic, In2aPin);
+Input in2b = Input(In2B_Topic, In2bPin);
+Input in3a = Input(In3A_Topic, In3aPin);
+Input in3b = Input(In3B_Topic, In3bPin);
+Input in4a = Input(In4A_Topic, In4aPin);
+Input in4b = Input(In4B_Topic, In4bPin);
+Input in5a = Input(In5A_Topic, In5aPin);
+Input in5b = Input(In5B_Topic, In5bPin);
+Input in6a = Input(In6A_Topic, In6aPin);
+Input in6b = Input(In6B_Topic, In6bPin);
+Input in7a = Input(In7A_Topic, In7aPin);
+Input in7b = Input(In7B_Topic, In7bPin);
+Input in8a = Input(In8A_Topic, In8aPin);
+Input in8b = Input(In8B_Topic, In8bPin);
+Input in9a = Input(In9A_Topic, In9aPin);
+Input in9b = Input(In9B_Topic, In9bPin);
---
+++ examples/mqtt/ct_all_sense/w5x00_mqtt_publish_all_sense.cpp
@@ -0,0 +1,399 @@
+/**
+ * Copyright (c) 2021 WIZnet Co.,Ltd
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Includes
+ * ----------------------------------------------------------------------------------------------------
+ */
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <sstream> 
+
+#include "port_common.h"
+
+#include "wizchip_conf.h"
+// #include "w5x00_spi.h" // covered by extern "C
+// #include "timer.h"     // covered by extern "C
+#include "mqtt_interface.h"
+#include "MQTTClient.h"
+
+#include "defines.h"
+
+/* pico include */
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
+#include "hardware/watchdog.h"
+/* pico include end */
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Variables
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Network */
+static wiz_NetInfo g_net_info =
+    {
+        .mac = MAC,                         // MAC address
+        .ip  = IP,                          // IP address
+        .sn  = SUBNET,                      // Subnet Mask
+        .gw  = GATEWAY,                     // Gateway
+        .dns = DNS,                         // DNS server
+        .dhcp = NETINFO_STATIC              // DHCP enable/disable
+};
+
+/* MQTT */
+static uint8_t g_mqtt_send_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_mqtt_recv_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_mqtt_broker_ip[4] = MQTT_SERVER;
+static Network g_mqtt_network;
+static MQTTClient g_mqtt_client;
+static MQTTPacket_connectData g_mqtt_packet_connect_data = MQTTPacket_connectData_initializer;
+static MQTTMessage g_mqtt_message;
+// Last Will Testament for MQTT
+static MQTTPacket_willOptions g_mqtt_will_options = MQTTPacket_willOptions_initializer;
+
+/* Timer  */
+static volatile uint32_t g_msec_cnt = 0;
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Functions
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+static void set_clock_khz(void);
+
+/* Timer  */
+static void repeating_timer_callback(void);
+static time_t millis(void);
+
+void networkConfig();
+void mqttConnect();
+bool sendMqtt(const string& topic, const string& data);
+bool sendMqtt(const string& topic, const int value);
+
+/* GPIO */
+void inputInit(uint8_t pinNo, gpio_irq_callback_t callback);
+void gpio_callback(uint gpio, uint32_t events);
+void processInput(Input& input, bool state);
+
+void log(const string& domain, const string& log) { std::cout << domain << ": " << log << std::endl; }
+void log(const string& domain, const uint32_t value) { std::cout << domain << ": " << value << std::endl; }
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Main
+ * ----------------------------------------------------------------------------------------------------
+ */
+int main()
+{
+    set_clock_khz();
+    stdio_init_all();
+
+    watchdog_enable(10000, false);
+
+    inputInit(in1a.pinNo, &gpio_callback);
+    inputInit(in1b.pinNo, &gpio_callback);
+    inputInit(in2a.pinNo, &gpio_callback);
+    inputInit(in2b.pinNo, &gpio_callback);
+    inputInit(in3a.pinNo, &gpio_callback);
+    inputInit(in3b.pinNo, &gpio_callback);
+    inputInit(in4a.pinNo, &gpio_callback);
+    inputInit(in4b.pinNo, &gpio_callback);
+    inputInit(in5a.pinNo, &gpio_callback);
+    inputInit(in5b.pinNo, &gpio_callback);
+    inputInit(in6a.pinNo, &gpio_callback);
+    inputInit(in6b.pinNo, &gpio_callback);
+    inputInit(in7a.pinNo, &gpio_callback);
+    inputInit(in7b.pinNo, &gpio_callback);
+    inputInit(in8a.pinNo, &gpio_callback);
+    inputInit(in8b.pinNo, &gpio_callback);
+    inputInit(in9a.pinNo, &gpio_callback);
+    inputInit(in9b.pinNo, &gpio_callback);
+
+    networkConfig();
+
+    /* Initialize */
+    int32_t retval = 0;
+    uint32_t start_ms = 0;
+    uint32_t end_ms = 0;
+    bool initDone = false;
+
+    start_ms = millis();
+
+    /* Infinite loop */
+    while (1)
+    {
+        if (g_mqtt_client.isconnected)
+        {
+            watchdog_update();
+
+            if (!initDone)
+            {
+                auto initRet = sendMqtt(willTopic, willMessageOn);
+                initDone = initRet;
+            }
+
+            bool result = true;
+            if ((retval = MQTTYield(&g_mqtt_client, g_mqtt_packet_connect_data.keepAliveInterval)) < 0)
+            {
+                log("MQTT", "Yield error: ");// + retval);
+                result = false;
+            }
+
+            end_ms = millis();
+
+            if (end_ms > start_ms + MQTT_PUBLISH_PERIOD)
+            {
+                result = sendMqtt(uptimeTopic, millis()/1000) && result;
+                start_ms = millis();
+            }
+
+            if (!result)
+            {
+                g_mqtt_client.isconnected = 0;
+            }
+        }
+        else
+        {
+            mqttConnect();
+            initDone = false;
+        }
+    }
+}
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Functions
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+static void set_clock_khz(void)
+{
+    // set a system clock frequency in khz
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+
+    // configure the specified clock
+    clock_configure(
+        clk_peri,
+        0,                                                // No glitchless mux
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
+        PLL_SYS_KHZ * 1000,                               // Input frequency
+        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
+    );
+}
+
+/* Timer */
+static void repeating_timer_callback(void)
+{
+    g_msec_cnt++;
+
+    MilliTimer_Handler();
+}
+
+static time_t millis(void)
+{
+    return g_msec_cnt;
+}
+
+/* GPIO */
+#define LEVEL_LOW  0x1
+#define LEVEL_HIGH 0x2
+#define EDGE_FALL  0x4
+#define EDGE_RISE  0x8
+
+void inputInit(uint8_t pinNo, gpio_irq_callback_t callback)
+{
+    gpio_init(pinNo);
+    gpio_set_dir(pinNo, GPIO_IN);
+    gpio_pull_up(pinNo);
+    gpio_set_irq_enabled_with_callback(pinNo, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, callback);
+}
+
+void gpio_callback(uint gpio, uint32_t events)
+{
+    bool state = events & EDGE_RISE ? true : false;
+    state = events & EDGE_FALL ? false : true;
+
+    if (gpio == in1a.pinNo) processInput(in1a, state);
+    else if (gpio == in1b.pinNo) processInput(in1b, state);
+    else if (gpio == in2a.pinNo) processInput(in2a, state);
+    else if (gpio == in2b.pinNo) processInput(in2b, state);
+    else if (gpio == in3a.pinNo) processInput(in3a, state);
+    else if (gpio == in3b.pinNo) processInput(in3b, state);
+    else if (gpio == in4a.pinNo) processInput(in4a, state);
+    else if (gpio == in4b.pinNo) processInput(in4b, state);
+    else if (gpio == in5a.pinNo) processInput(in5a, state);
+    else if (gpio == in5b.pinNo) processInput(in5b, state);
+    else if (gpio == in6a.pinNo) processInput(in6a, state);
+    else if (gpio == in6b.pinNo) processInput(in6b, state);
+    else if (gpio == in7a.pinNo) processInput(in7a, state);
+    else if (gpio == in7b.pinNo) processInput(in7b, state);
+    else if (gpio == in8a.pinNo) processInput(in8a, state);
+    else if (gpio == in8b.pinNo) processInput(in8b, state);
+    else if (gpio == in9a.pinNo) processInput(in9a, state);
+    else if (gpio == in9b.pinNo) processInput(in9b, state);
+}
+
+void processInput(Input& input, bool state)
+{
+    input.measuredValue = state;
+    if (input.measuredValue != input.lastReportedValue || !input.initDone)
+    {
+        string mqttState = state ? "HIGH" : "LOW";
+        log("input", input.topicStat);
+        log("input", state);
+        auto result = sendMqtt(input.topicStat, mqttState);
+        if (result)
+        {
+            input.lastReportedValue = input.measuredValue;
+            input.initDone = true;
+        }
+    }
+}
+
+// MQTT
+extern "C" void wizchip_spi_initialize(void);
+extern "C" void wizchip_cris_initialize(void);
+extern "C" void wizchip_reset(void);
+extern "C" void wizchip_initialize(void);
+extern "C" void wizchip_check(void);
+extern "C" void network_initialize(wiz_NetInfo net_info);
+extern "C" void print_network_information(wiz_NetInfo net_info);
+extern "C" void wizchip_1ms_timer_initialize(void (*callback)(void));
+
+///////////===================================================================
+
+void networkConfig()
+{
+    wizchip_spi_initialize();
+    wizchip_cris_initialize();
+
+    wizchip_reset();
+    wizchip_initialize();
+    wizchip_check();
+
+    wizchip_1ms_timer_initialize(repeating_timer_callback);
+    network_initialize(g_net_info);
+
+    /* Get network information */
+    print_network_information(g_net_info);
+
+    NewNetwork(&g_mqtt_network, SOCKET_MQTT);
+}
+
+string getNetInfo(wiz_NetInfo net_info)
+{
+    stringstream stream;
+    ctlnetwork(CN_GET_NETINFO, (void *)&net_info);
+
+    if (net_info.dhcp == NETINFO_DHCP)
+    {
+        stream << "network config : DHCP\n";
+    }
+    else
+    {
+        stream << "network config : static\n";
+    }
+
+    stream << " MAC         : " << net_info.mac << "\n";
+    // printf(" IP          : %d.%d.%d.%d\n", net_info.ip[0], net_info.ip[1], net_info.ip[2], net_info.ip[3]);
+    // printf(" Subnet Mask : %d.%d.%d.%d\n", net_info.sn[0], net_info.sn[1], net_info.sn[2], net_info.sn[3]);
+    // printf(" Gateway     : %d.%d.%d.%d\n", net_info.gw[0], net_info.gw[1], net_info.gw[2], net_info.gw[3]);
+    // printf(" DNS         : %d.%d.%d.%d\n", net_info.dns[0], net_info.dns[1], net_info.dns[2], net_info.dns[3]);
+    string str;
+    stream >> str;
+    return str;
+}
+
+void mqttConnect()
+{
+    int32_t retval = 0;
+
+    retval = ConnectNetwork(&g_mqtt_network, g_mqtt_broker_ip, MQTT_PORT);
+
+    if (retval != 1)
+    {
+        log("Network", "Network connect failed");
+        return;
+    }
+
+    log("MQTT", "mqttConnect start");
+    /* Initialize MQTT client */
+    MQTTClientInit(&g_mqtt_client, &g_mqtt_network, DEFAULT_TIMEOUT, g_mqtt_send_buf, ETHERNET_BUF_MAX_SIZE, g_mqtt_recv_buf, ETHERNET_BUF_MAX_SIZE);
+
+    /* Connect to the MQTT broker */
+    g_mqtt_packet_connect_data.MQTTVersion = 3;
+    g_mqtt_packet_connect_data.cleansession = 1;
+    g_mqtt_packet_connect_data.willFlag = 1;
+    g_mqtt_packet_connect_data.keepAliveInterval = MQTT_KEEP_ALIVE;
+    g_mqtt_packet_connect_data.clientID.cstring = const_cast<char*>(MQTT_CLIENT_ID.c_str());
+    g_mqtt_packet_connect_data.username.cstring = const_cast<char*>(MQTT_USERNAME.c_str());
+    g_mqtt_packet_connect_data.password.cstring = const_cast<char*>(MQTT_PASSWORD.c_str());
+    g_mqtt_packet_connect_data.will = g_mqtt_will_options;
+    g_mqtt_will_options.topicName.cstring = const_cast<char*>(willTopic.c_str());
+    g_mqtt_will_options.message.cstring   = const_cast<char*>(willMessageOff.c_str());
+    g_mqtt_will_options.retained = 1;
+    
+    retval = MQTTConnect(&g_mqtt_client, &g_mqtt_packet_connect_data);
+
+    if (retval < 0)
+    {
+        log("MQTT", "MQTT connect failed: ");// + retval);
+        return;
+    }
+
+    log("MQTT", "MQTT connected");
+
+    sendMqtt(statusTopic, getNetInfo(g_net_info));
+
+    if (retval < 0)
+    {
+        log("MQTT", "Subscribe failed: ");//+ retval);
+        return;
+    }
+
+    log("MQTT", "Subscribed");
+}
+
+bool sendMqtt(const string& topic, const string& data)
+{
+    /* Configure publish message */
+    g_mqtt_message.qos = QOS0;
+    g_mqtt_message.retained = 0;
+    g_mqtt_message.dup = 0;
+    g_mqtt_message.payload = const_cast<char*>(data.c_str());
+    g_mqtt_message.payloadlen = strlen(data.c_str());
+
+    int32_t retval = 0;
+    /* Publish */
+    retval = MQTTPublish(&g_mqtt_client, topic.c_str(), &g_mqtt_message);
+    if (retval < 0)
+    {
+        log("MQTT", "Publish failed");
+        return false;
+    }
+    return true;
+}
+
+bool sendMqtt(const string& topic, const int value)
+{
+  char dataChar[6];
+  itoa(value, dataChar, 10);
+  const string dataStr(dataChar);
+  return sendMqtt(topic, dataStr);
+}
+
